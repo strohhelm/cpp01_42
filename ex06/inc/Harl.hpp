baeba57e45634f3e846eacd9a0ccaf7011f5dfe6
pstrohal <pstrohal@student.42heilbronn.de>
2024-11-20 16:02:27 +0100

    added colours to ex06

--- ex06/inc/Harl.hpp
+++ ex06/inc/Harl.hpp
@@ -6,12 +6,21 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 19:57:17 by pstrohal          #+#    #+#             */
-/*   Updated: 2024/11/20 11:31:38 by pstrohal         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:00:29 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 #define	HARL_HPP
+
+#define X   "\033[0m"
+#define R   "\033[31m"
+#define G   "\033[32m"
+#define Y   "\033[33m"
+#define B   "\033[34m"
+#define M   "\033[35m"
+#define C   "\033[36m"
+#define W   "\033[37m"
 
 #include <iostream>
 #include <string>
--- ex06/src/Harl.cpp
+++ ex06/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:18:57 by pstrohal          #+#    #+#             */
-/*   Updated: 2024/11/20 12:10:21 by pstrohal         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:01:24 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,26 +37,26 @@ void	Harl::complain(int level)
 			(this->*func[3])();
 			break ;
 		default :
-			std::cout<<"[ Probably complaining about insignificant problems ]\n"<<std::endl;
+			std::cout<<B<<"[ Probably complaining about insignificant problems ]\n"<<X<<std::endl;
 	}
 }
 
 void	Harl::debug(void)
 {
-	std::cout<<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!\n"<<std::endl;
+	std::cout<<G<<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!\n"<<X<<std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout<<"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n"<<std::endl;
+	std::cout<<Y<<"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n"<<X<<std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout<<"I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.this\n"<<std::endl;
+	std::cout<<M<<"I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.this\n"<<X<<std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout<<"This is unacceptable! I want to speak to the manager now.\n"<<std::endl;
+	std::cout<<R<<"This is unacceptable! I want to speak to the manager now.\n"<<X<<std::endl;
 }
