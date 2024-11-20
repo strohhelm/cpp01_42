bcbade3408d79973b3cf3a3b359e2ad7577a0a25
pstrohal <pstrohal@student.42heilbronn.de>
2024-11-20 16:24:25 +0100

    added test to ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,25 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 12:50:33 by pstrohal          #+#    #+#             */
-/*   Updated: 2024/11/18 13:26:25 by pstrohal         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:23:36 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef COLOURS_MAIN
+#define COLOURS_MAIN
+#define X   "\033[0m"
+#define R   "\033[31m"
+#define G   "\033[32m"
+#define Y   "\033[33m"
+#define B   "\033[34m"
+#define M   "\033[35m"
+#define C   "\033[36m"
+#define W   "\033[37m"
+#endif
+
 #include <iostream>
 #include <string>
-
 int main(void)
 {
 	std::string brain = "HI THIS IS BRAIN";
@@ -20,13 +32,20 @@ int main(void)
 	std::string &strREF = brain;
 
 	std::cout<<std::endl;
-	std::cout	<< &brain	<<"\n"
-				<<strPTR	<<"\n"
-				<<&strREF	<<"\n\n"
-				<<std::endl;
-	std::cout	<<brain		<<"\n"
-				<<*strPTR	<<"\n"
-				<<strREF	<<"\n\n"
-				<<std::endl;
+	std::cout	<<Y<<&brain		<<"\n"
+				<<G<<strPTR		<<"\n"
+				<<M<<&strREF	<<"\n\n"
+				<<X<<std::endl;
+				
+	std::cout	<<Y<<brain		<<"\n"
+				<<G<<*strPTR	<<"\n"
+				<<M<<strREF		<<"\n\n"
+				<<X<<std::endl;
+
+	std::cout	<<Y<<brain		<<"\n"
+				<<G<<strPTR		<<"\n"
+				<<M<<strREF		<<"\n\n"
+				<<X<<std::endl;
+
 	return 0;
 }
